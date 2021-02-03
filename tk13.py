f2b7368753d0ddc1cf02237caa079fca0561c154
ryuuichi <ap19090@azec.ac.jp>
2021-02-03 14:02:41 +0900

    username and email

--- tk13.py
+++ tk13.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("350x250")
+root.geometry("400x200")
 lb = tk.Label(text="This is a LabelThis is a Label,This is a Label")
 ms = tk.Message(text="This is a Message,This is a Message,This is a Message")
 [widget.pack() for widget in (lb,ms)]
